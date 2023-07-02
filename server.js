36c2f4c531049e2f1f394465559ce163310f0e6b
Valentin <valentin.grenier02@gmail.com>
2023-07-02 22:17:43 +0200

    Remove Github action

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
   );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   next();
 });
 
