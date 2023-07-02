a570ea3a78b7aa890d3dbec672000f188488b552
valentin-grenier <valentin.grenier02@gmail.com>
2023-07-02 13:37:18 +0200

    Animation on show, format date

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -15,7 +15,7 @@ interface DataTypes {
   prenom_1: string | undefined;
   nom: string | undefined;
   categorie_entreprise: string | undefined;
-  date_creation: number | undefined;
+  date_creation: Date;
   etablissement_siege: {
     enseigne_1: string | undefined;
     numero_voie: number | undefined;
@@ -33,56 +33,89 @@ interface ResultProps {
 const Result = ({ searchResult }: ResultProps) => {
   const data = searchResult;
 
+  const formatDate = () => {
+    if (data) {
+      const isoDate = new Date(data.date_creation);
+
+      const day = String(isoDate.getDate()).padStart(2, '0');
+      const month = String(isoDate.getMonth() + 1).padStart(2, '0');
+      const year = String(isoDate.getFullYear()).padStart(2, '0');
+
+      return `${day}/${month}/${year}`;
+    }
+  };
+
+  const date = formatDate();
+
   useEffect(() => {}, [searchResult]);
 
   return (
     <div>
       {data ? (
         <div className="result">
-          <div id="siret">
-            <h3>
-              <MdOutlineNumbers />
-              SIREN
-            </h3>
-            <p>{data.siren}</p>
-          </div>
-          <div id="owner">
-            <h3>
-              <MdPermContactCalendar />
-              Gérant
-            </h3>
-            <p>
-              {data.prenom_1} {data.nom}
-            </p>
-          </div>
-          <div id="type">
-            <h3>
-              <MdBusiness />
-              Type d'entreprise
-            </h3>
-            <p>{data.categorie_entreprise}</p>
-          </div>
-          <div id="creation-date">
-            <h3>
-              <MdDateRange />
-              Date de création
-            </h3>
-            <p>{data.date_creation}</p>
-          </div>
-          <div id="name">
-            <h3>
-              <MdOutlineDriveFileRenameOutline />
-              Nom de l'enseigne
-            </h3>
-            <p>{data.etablissement_siege.enseigne_1}</p>
-          </div>
-          <div id="address">
-            <h3>
-              <MdHome />
-              Adresse du siège
-            </h3>
-            <p>{`${data.etablissement_siege.numero_voie} ${data.etablissement_siege.type_voie} ${data.etablissement_siege.libelle_voie}, ${data.etablissement_siege.code_postal} ${data.etablissement_siege.libelle_commune}`}</p>
-          </div>
+          {data.siren !== null && (
+            <div id="siren">
+              <h3>
+                <MdOutlineNumbers />
+                SIREN
+              </h3>
+              <p>{data.siren}</p>
+            </div>
+          )}
+
+          {(data.prenom_1 !== null || data.nom !== null) && (
+            <div id="owner">
+              <h3>
+                <MdPermContactCalendar />
+                Gérant
+              </h3>
+              <p>
+                {data.prenom_1} {data.nom}
+              </p>
+            </div>
+          )}
+
+          {data.categorie_entreprise !== null && (
+            <div id="type">
+              <h3>
+                <MdBusiness />
+                Type d'entreprise
+              </h3>
+              <p>{data.categorie_entreprise}</p>
+            </div>
+          )}
+
+          {data.date_creation !== null && (
+            <div id="creation-date">
+              <h3>
+                <MdDateRange />
+                Date de création
+              </h3>
+              <p>{date}</p>
+            </div>
+          )}
+
+          {data.etablissement_siege.enseigne_1 !== null && (
+            <div id="name">
+              <h3>
+                <MdOutlineDriveFileRenameOutline />
+                Nom de l'enseigne
+              </h3>
+              <p>{data.etablissement_siege.enseigne_1}</p>
+            </div>
+          )}
+
+          {data.etablissement_siege.libelle_commune !== null && (
+            <div id="address">
+              <h3>
+                <MdHome />
+                Adresse du siège
+              </h3>
+              <p>
+                {`${data.etablissement_siege.numero_voie} ${data.etablissement_siege.type_voie} ${data.etablissement_siege.libelle_voie}, ${data.etablissement_siege.code_postal} ${data.etablissement_siege.libelle_commune}`.toLowerCase()}
+              </p>
+            </div>
+          )}
         </div>
       ) : (
         ''
