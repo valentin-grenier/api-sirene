12d7d766ad7447b4a3a99d679651433801178358
valentin-grenier <valentin.grenier02@gmail.com>
2023-07-02 12:11:36 +0200

    Edit components to get the API data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,32 @@ import './App.scss';
 import Header from '../Header/Header';
 import SearchBar from '../SearchBar/SearchBar';
 import Result from '../Result/Result';
+import { useState } from 'react';
 
 const App = () => {
+  const [siren, setSiren] = useState('');
+  const [searchResult, setSearchResult] = useState({});
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Get the data from the API through Express server
+  const fetchAPI = async () => {
+    fetch(`http://localhost:3000/api/siren/${siren}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResult(data.unite_legale);
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMessage('Une erreur est survenue');
+      });
+  };
+
   return (
     <div className="App">
       <div className="header">
         <Header />
-        <SearchBar />
+        <SearchBar fetchAPI={fetchAPI} siren={siren} setSiren={setSiren} />
       </div>
       <Result />
     </div>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,42 +1,23 @@
 // import { SetStateAction, useState } from 'react';
 import './SearchBar.scss';
 import { BsSearch } from 'react-icons/bs';
-import axios from 'axios';
 import { useState } from 'react';
 
-const SearchBar = () => {
-  // State input value
-  const [inputValue, setInputValue] = useState('840004394');
+interface SearchBarProps {
+  fetchAPI: Function;
+  siren: string;
+  setSiren: Function;
+}
 
-  // API
-  const apiKey = 'Au7jZeGsdNBQ6oPYJI0ocx6uDzFghlwY';
-  const apiUrl = `https://data.siren-api.fr/v3/unites_legales/${inputValue}`;
-
-  const getData = async () => {
-    try {
-      const response = await axios.get(apiUrl, {
-        headers: {
-          'X-Client-Secret': apiKey,
-        },
-      });
-      console.log(response);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données : ' + error);
-    }
-  };
-
+const SearchBar = ({ fetchAPI, siren, setSiren }: SearchBarProps) => {
   // Handle input change
-  const handleChange = (event: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    setInputValue(event.target.value);
+  const handleChange = (event) => {
+    setSiren(event.target.value);
   };
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
-
-    // Get data from the API
-    getData();
+    fetchAPI();
   };
 
   return (
@@ -53,7 +34,7 @@ const SearchBar = () => {
             type="search"
             name="search-siret"
             placeholder="Numéro SIRET"
-            value={inputValue}
+            value={siren}
             onChange={handleChange}
             maxLength={9}
           />
